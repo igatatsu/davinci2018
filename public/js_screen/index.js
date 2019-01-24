ee8b285e97cac630a8ae0e54d15b4f19a8650c87
igatatsu <45650088+igatatsu@users.noreply.github.com>
2019-01-24 10:06:56 +0900

    Update index.js

--- public/js_screen/index.js
+++ public/js_screen/index.js
@@ -311,7 +311,7 @@ Circle.prototype = {
     let futureLocY = this.locY + distanceY;
 
     // 左右衝突判定
-    if (futureLocX < this.radius || futureLocX > this.width-this.radius) {
+    if (futureLocX < this.radius+1 || futureLocX > this.width-this.radius-1) {
       futureLocX -= distanceX;  // 進んだ分を戻す
       this.direction = 180 - this.direction; // 角度変更
       radian = this.direction * Math.PI / 180; // ラジアンへ変換
@@ -320,7 +320,7 @@ Circle.prototype = {
     }
 
     // 上下衝突判定
-    if (futureLocY < this.radius || futureLocY > this.height-this.radius) {
+    if (futureLocY < this.radius+1 || futureLocY > this.height-this.radius-1) {
       futureLocY -= distanceY;
       this.direction = 360 - this.direction;
       radian = this.direction * Math.PI / 180;
